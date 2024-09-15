e876903fd10e8cc93c70ea5e9da6e2a220b1bea7
Suraj-Hudda <surajhudda17@gmail.com>
2024-09-15 17:07:21 +0530

    completed all module

--- src/constant/__init.py
+++ src/constant/__init.py
@@ -5,8 +5,11 @@ AWS_S3__BUCKET_NAME="wafer-fault"
 MONGO_DATABASE_NAME="mlprojects"
 MONGO_COLLECTION_NAME="waferfault"
 
-TARGET-COLUMN="quality"
-MONGO_DB_URL=""
+TARGET_COLUMN="quality"
+MONGO_DB_URL="mongodb+srv://surajhudda21:Hudda@1000@cluster0.oeec2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 
 MODEL_FILE_NAME="model"
 MODEL_FILE_EXTENSION=".pkl"
+
+ARTIFACTS_FOLDER="artifacts"
